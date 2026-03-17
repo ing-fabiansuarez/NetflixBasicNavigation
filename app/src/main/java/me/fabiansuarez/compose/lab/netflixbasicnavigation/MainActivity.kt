cbbea991781eb14dd2e5f5e7fcc632f6e4f7e6eb
Fabian Enrique Suarez Carvajal <77165745+ing-fabiansuarez@users.noreply.github.com>
2026-03-16 20:46:08 -0500

    feat: Basic use of Jetpack compose navigation

--- app/src/main/java/me/fabiansuarez/compose/lab/netflixbasicnavigation/MainActivity.kt
+++ app/src/main/java/me/fabiansuarez/compose/lab/netflixbasicnavigation/MainActivity.kt
@@ -11,6 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.screen.HomeScreen
+import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.screen.LoginScreen
+import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.screen.RegisterScreen
+import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.screen.RegisterStep2Screen
+import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.screen.RegisterStep3Screen
 import me.fabiansuarez.compose.lab.netflixbasicnavigation.ui.theme.NetflixBasicNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +26,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "register_step_2") {
+                composable("login") {
+                    LoginScreen()
+                }
+                composable("register_step_1") {
+                    RegisterScreen()
+                }
+                composable("register_step_2") {
+                    RegisterStep2Screen()
+                }
+                composable("register_step_3") {
+                    RegisterStep3Screen()
+                }
+                composable("home") {
+                    HomeScreen()
+                }
+            }
         }
     }
 }
